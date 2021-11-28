0be5bc85455ec0a49e4242f2568f0b50ed5f6b3f
Daniel Sutton <daniel.sutton@slimmingworld.co.uk>
2021-11-28 10:23:32 +0000

    Updated Snyk to 1.775.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $fileLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   PackageName  = $env:ChocolateyPackageName
   FileFullPath = "$fileLocation\snyk.exe"
-  Url          = 'https://github.com/snyk/snyk/releases/download/v1.366.0/snyk-win.exe'
-  Checksum     = '8f584d492d11a5f95d2a7886bfa54bbb93d402a437c89a021708414220a9f934'
+  Url          = 'https://github.com/snyk/snyk/releases/download/v1.775.0/snyk-win.exe'
+  Checksum     = '318f55b33dd2646c18109d7aa7da3a78d5063fedb6c099087d6aad74d1110d98'
   ChecksumType = 'sha256'
 }
 
